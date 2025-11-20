25d57d7070bf6d7b8602fc468cfef0221386cdfe
TRAN VAN DUC <duc30072004@gmail.com>
2025-11-20 10:37:43 +0700

    render frontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Trong Docker, gọi API qua nginx proxy
 // Ngoài Docker, gọi trực tiếp backend
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+const API_URL = process.env.REACT_APP_API_URL || 'https://devops-1-9r3z.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
